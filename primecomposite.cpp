ff5f66f69c7c3b0c8171140ba79e0c796f4f74f7
Abik061 <58253996+Abik061@users.noreply.github.com>
2019-11-27 15:52:00 +0545

    Add files via upload

---
+++ primecomposite.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,i = 1,a = 0;
+
+    cout << "Enter a number : ";
+    cin >> n;
+
+    for(i;i<=n;i++)
+    {
+        if (n % i == 0)
+        {
+            a = a + 1;
+        }
+    }
+
+    if(a==2)
+    {
+
+        cout << n << " is prime." << endl;
+
+    }else{
+
+        cout << n << " is composite." << endl;
+
+    }
+
+    return 0;
+}

cad42b6826c616a6a6219ed258b24bc02de4d906
Abik061 <58253996+Abik061@users.noreply.github.com>
2019-11-29 11:54:30 +0545

    Add files via upload

---
+++ even_numbers_upto_20.cpp
@@ -0,0 +1,15 @@
+// to print even numbers upto 20.
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 0;
+
+    for(a;a<=20;a+=2)
+    {
+        cout << a << endl;
+    }
+    return 0;
+}
---
+++ odd_numbers_upto_20.cpp
@@ -0,0 +1,15 @@
+//to print odd numbers upto 20.
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 1;
+
+    for(i;i<=20;i+=2)
+    {
+        cout << i << endl;
+    }
+    return 0;
+}
